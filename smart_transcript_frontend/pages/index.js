80fff291d1afeaff2db127fab516c30f3f6432b4
Adesdesk <aadelakun28@gmail.com>
2023-02-13 18:46:15 +0100

    Update index.js

--- smart_transcript_frontend/pages/index.js
+++ smart_transcript_frontend/pages/index.js
@@ -8,7 +8,7 @@ import { Inter } from '@next/font/google';
 import Footer from '../components/Footer';
 import Navbar from '../components/Navbar';
 
-
+// apply Inter font
 const inter = Inter({ subsets: ['latin'] })
 
 const Home = () => {
