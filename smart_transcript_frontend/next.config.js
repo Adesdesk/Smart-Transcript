d2f2fd0f769e8f626cdb41399319c4049bebea1a
Adesdesk <aadelakun28@gmail.com>
2023-02-21 19:30:09 +0100

    Revert "Update next.config.js"
    This reverts commit 91c78a56845ea5f74d53f21ad9e45a37350d32d7.

--- smart_transcript_frontend/next.config.js
+++ smart_transcript_frontend/next.config.js
@@ -1,20 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // environment variables
-  env: {
-<<<<<<< HEAD
-=======
-    NEXT_PUBLIC_PINATA_API_KEY: 'b0b8a0096bc4b4957cc4',
-    NEXT_PUBLIC_PINATA_SECRET: 'e8ff08a04ce87e3171d5482fe6aae97ad6c60cc15ec72889aaccfe4f4b95947d',
-    NEXT_PUBLIC_ALCHEMY_KEY: 'https://eth-goerli.g.alchemy.com/v2/zDVopI9bKu4AMOsJc6RnlYfReVlfSRCH',
->>>>>>> parent of 700b536 (Update next.config.js)
-
-    NEXT_PUBLIC_PINATA_API_KEY: 'b0b8a0096bc4b4957cc4';
-    NEXT_PUBLIC_PINATA_SECRET: 'e8ff08a04ce87e3171d5482fe6aae97ad6c60cc15ec72889aaccfe4f4b95947d';
-    NEXT_PUBLIC_ALCHEMY_KEY: 'https://eth-goerli.g.alchemy.com/v2/zDVopI9bKu4AMOsJc6RnlYfReVlfSRCH';
+  // abstracted environment variables
+  /*env: {
     
-  },
+  },*/
 }
 
 module.exports = nextConfig
